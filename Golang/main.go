8087818ef83aea9e8810bfd69dd4532444467f58
akhrf <memskie324@gmail.com>
2025-05-08 21:12:36 +0700

    a

--- Golang/main.go
+++ Golang/main.go
@@ -6,7 +6,7 @@ func main() {
  app := fiber.New()
 
  app.Get("/", func(c *fiber.Ctx) error {
-  return c.SendString("Hallo, Dunia")
+  return c.SendString("Hallo, Dunia ")
  })
 
  app.Listen(":3000")
